0a5382c5b606ead50cc02ce8ded4094a51b1591f
Shanthee Sampath <shantheesam@gmail.com>
2017-01-03 15:31:40 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// function def for calculating area of circle 
+function calculate_area(radius) {
+	area = Math.PI * radius * radius;
+	return area;
+}
+
+//to print on the console
+var area = calculate_area(5);
+console.log(area);
+
